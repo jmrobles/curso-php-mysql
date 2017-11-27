9787424acd60652795c3d748cf571e754bc24ec3
chemaoutbarriers <chema@outbarriers.com>
2017-11-27 16:46:10 +0100

    Se añaden ejemplos de funciones

--- ejemplos/funciones1.php
+++ ejemplos/funciones1.php
@@ -1,5 +1,23 @@
 <?php
+// Declaración/definición de función
+function addIVA($numero) {
+  return $numero * 1.21;
+}
+// Esto es procedimiento
+function saluda() {
+  echo "Hola a todos<br>";
+  echo "¿Como vais?";
+}
 
-echo "veamos las funciones";
+function addIVAMejorado($valor, $factor=1.21) {
+  return $valor * $factor;
+}
 
+saluda();
+$resultado = addIVA(8);
+echo $resultado;
+echo "<br>";
+echo addIVAMejorado(10);
+echo "<br>";
+echo addIVAMejorado(10, 1.0);
 ?>
